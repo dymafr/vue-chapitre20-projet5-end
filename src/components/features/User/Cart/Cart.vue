0e8d9e864aa754d9a47af192fbff2ab21311518d
hades <quentindevial19@gmail.com>
2018-11-11 01:04:47 +0100

    first commit

---
+++ src/components/features/User/Cart/Cart.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <cart-products-list :cart="cart" class="p-3"></cart-products-list>
+  </div>
+</template>
+
+<script>
+  import CartProductsList from './CartProductsList';
+  import { eventBus } from '../../../../main';
+
+export default {
+  components: {
+    CartProductsList
+  },
+  data() {
+    return {
+      cart: []
+    }
+  },
+  created() {
+    this.cart = eventBus.cart;
+    eventBus.$on('update:cart', (cart) => {
+      this.cart = cart;
+    })
+  }
+}
+
+</script>
+
+<style>
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,59 @@
+import Vue from 'vue'
+import App from './App.vue'
+import * as Filters from './utils/filters';
+import router from './router';
+import axios from 'axios';
+
+Vue.config.productionTip = false;
+axios.defaults.baseURL = 'https://boutique-72791.firebaseio.com/';
+Vue.prototype.$http = axios;
+
+
+Object.keys(Filters).forEach( (f) => {
+  Vue.filter(f, Filters[f]);
+})
+
+export const eventBus = new Vue({
+  data: {
+    products: [],
+    cart: [],
+  },
+  methods: {
+    addProductToCart(product) {
+      if (!this.cart.map( i => i.id).includes(product.id)) {
+        this.cart = [ ...this.cart, product ];
+        this.$emit('update:cart', this.cart.slice());
+      }
+    },
+    removeItemFromCart(item) {
+      this.cart = this.cart.slice().filter( i => i.id !== item.id );
+      this.$emit('update:cart', this.cart.slice());
+    },
+    addProduct(product) {
+      this.$http.post('products.json', product)
+                .then( res => {
+                  this.products = [ ...this.products, { ...product, id: this.products.length + 1 + '' }],
+                  this.$emit('update:products', this.products);
+                })
+    },
+    addProducts(products) {
+      this.products = products;
+      this.$emit('update:products', this.products);
+    },
+    initProducts() {
+      this.$http.get('products.json')
+                .then( res => {
+                  const data = res.data;
+                  this.addProducts(Object.keys(data).map( key => data[key]));
+                });
+    }
+  },
+  created() {
+    this.initProducts();
+  }
+})
+
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount('#app')
